37020264b99261c16d90b3ea5edd627e25290372
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2021-09-10 22:17:17 +0900

    17427 약수의 합 2 .cpp WRONG
    아이디어
    1 ~ N까지 1을 약수로 가지는 수 N개, 2를 약수로 가지는 수 N/2개, ...

--- 코딩테스트 준비 - 기초/수학/17427.cpp
+++ 코딩테스트 준비 - 기초/수학/17427.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int Sum_of_divisor(int x)
-{
-	int sum = 0;
-
-	for (int i = 1; i <= x; ++i)
-	{
-		if (x % i == 0) sum += i;
-	}
-	return sum;
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -21,8 +10,13 @@ int main() {
 	
 	cin >> N;
 
-	for (int i = 1; i <= N; ++i) ans += Sum_of_divisor(i);
+	for (int i = 1; i <= N; ++i) ans += i * (N / i);
 
 	cout << ans;
 	return 0;
 }
+
+/* 아이디어 */
+// 1 ~ N까지 1을 약수로 가지는 수 N개
+//           2를 약수로 가지는 수 N/2개
+//            ....
