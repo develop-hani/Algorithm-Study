1fd4fd503ee64b06204488630f1e973c602b1a43
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2021-09-10 18:28:00 +0900

    10430 나머지.cpp

---
+++ 코딩테스트 준비 - 기초/수학/10430.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int A, B, C;
+
+	cin >> A >> B >> C;
+
+	cout << (A + B) % C << '\n';
+	cout << ((A % C) + (B % C)) % C << '\n';
+	cout << (A * B) % C << '\n';
+	cout << ((A % C) * (B % C)) % C << '\n';
+	
+	return 0;
+}
