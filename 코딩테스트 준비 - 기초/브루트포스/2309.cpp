d3a554e4fef6eb4b4a951dd31eb1fef5ab587992
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2021-09-14 14:53:46 +0900

    2309 일곱 난쟁이

---
+++ 코딩테스트 준비 - 기초/브루트포스/2309.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dwarf[9];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+
+	int sum_of_ninedwarf = 0;
+	int sum_of_sevendwarf;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		cin >> dwarf[i];
+		sum_of_ninedwarf += dwarf[i];
+	}
+
+	sort(dwarf, dwarf + 9);
+
+	for (int pick1 = 0; pick1 < 8; ++pick1)
+	{
+		for (int pick2 = pick1 + 1; pick2 < 9; ++pick2)
+		{
+			sum_of_sevendwarf = sum_of_ninedwarf - dwarf[pick1] - dwarf[pick2];
+			if (sum_of_sevendwarf == 100)
+			{
+				for (int i = 0; i < 9; ++i)
+					if (i != pick1 && i != pick2) cout << dwarf[i] << '\n';
+				return 0;
+			}
+		}
+	}
+
+
+	return 0;
+}
+
+/* 아이디어 */
+// 아홉난쟁이의 합에서 두 난쟁이를 선택해 빼기
