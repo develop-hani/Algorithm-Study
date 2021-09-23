b19486f05e646f0f1353a67215aaefab42619895
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2021-09-23 17:28:58 +0900

    성공

---
+++ 코딩테스트 준비 - 기초/부르트포스 - N과M/15654 N과M(5).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> input;
+vector<int> v;
+bool visited[8];
+
+void backtracking(int level)
+{
+	if (level == M)
+	{
+		for (int i = 0; i < v.size(); ++i)
+			cout << v[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= N; ++i)
+	{
+		if (!visited[i]) {
+			visited[i] = true;
+			v.push_back(input[i]);
+			backtracking(level + 1);
+			v.pop_back();
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N >> M;
+	input.push_back(0);
+	for (int i = 0; i < N; ++i)
+	{
+		int tmp;
+		cin >> tmp;
+		input.push_back(tmp);
+	}
+	sort(input.begin(), input.end());
+
+	backtracking(0);
+
+	return 0;
+}
+
+/* 아이디어 */
+// N과M(1)과 비슷하다고 생각해서 (1)의 알고리즘을 그대로 가져가고자 하였다.
+// 하지만 visited를 i=1부터 탐색하므로
+// 37행: input[0]에 가장 작은 값 0을 넣어 sort가 되더라도 영향을 미치지 않도록 하였다.
