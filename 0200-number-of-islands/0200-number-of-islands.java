91d3ca33339ab48ea22a89e59ea1d70cac269615
Park Yehan <parkyehan2060@gmail.com>
2026-07-06 21:58:27 +0900

    Time: 5 ms (29.28%), Space: 51.4 MB (85.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,43 +2,40 @@ class Solution {
     public int numIslands(char[][] grid) {
         int cnt = 0;
         int m = grid.length; int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (visited[i][j]) continue;
                 if (grid[i][j] == '0') continue;
 
                 cnt++;
-                bfs(visited, grid, i, j, m, n);
+                bfs(grid, i, j, m, n);
             }
         }
 
         return cnt;
     }
 
-    private void bfs(boolean[][] visited, char[][] grid, int i, int j, int m, int n) {
+    private void bfs(char[][] grid, int i, int j, int m, int n) {
         Queue<Node> q = new LinkedList<>();
 
         q.offer(new Node(i, j));
-        visited[i][j] = true;
+        grid[i][j] = '0';
 
         while (!q.isEmpty()) {
             Node cur = q.poll();
-            searchNeighbors(q, visited, grid, cur.i - 1, cur.j, m , n);
-            searchNeighbors(q, visited, grid, cur.i, cur.j + 1, m , n);
-            searchNeighbors(q, visited, grid, cur.i + 1, cur.j, m , n);
-            searchNeighbors(q, visited, grid, cur.i, cur.j - 1, m , n);
+            searchNeighbors(q, grid, cur.i - 1, cur.j, m , n);
+            searchNeighbors(q, grid, cur.i, cur.j + 1, m , n);
+            searchNeighbors(q, grid, cur.i + 1, cur.j, m , n);
+            searchNeighbors(q, grid, cur.i, cur.j - 1, m , n);
         }
     }
 
-    private boolean searchNeighbors(Queue<Node> q, boolean[][] visited, char[][] grid, int i, int j, int m, int n) {
+    private boolean searchNeighbors(Queue<Node> q, char[][] grid, int i, int j, int m, int n) {
         if (i < 0 || j < 0 || i >= m || j >= n) return false;
-        if (visited[i][j]) return false;
         if (grid[i][j] == '0') return false;
 
         q.offer(new Node(i, j));
-        visited[i][j] = true;
+        grid[i][j] = '0';
         return true;
     }
 
